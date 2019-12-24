c46ac22e3d5dc23ebbe9b800a8e9de67de926288
ykim5470-Lewis <58319204+ykim5470-Lewis@users.noreply.github.com>
2019-12-24 11:36:40 +0900

    Footer commit complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+import Footer from "./Pages/Footer/Footer";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Footer />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
