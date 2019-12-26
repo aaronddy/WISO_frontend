7828ee5f00b07d60934c1a75a75a31143e49e966
daye Kang <aaronddy94@gmail.com>
2019-12-26 21:42:34 +0900

    2차 병합

--- src/Routes.js
+++ src/Routes.js
@@ -2,11 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Reservation from "./Pages/Reservation/Reservation";
-<<<<<<< HEAD
 import SignIn from "./Pages/SignIn/SignIn";
-=======
-import Footer from "./Component/Footer/Footer";
->>>>>>> 0dceebf15bf4c53cec96268ecacfc08bc82db38a
+import Footer from "./Components/Footer/Footer";
 
 class Routes extends React.Component {
   render() {
@@ -15,11 +12,8 @@ class Routes extends React.Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/Reservation" component={Reservation} />
-<<<<<<< HEAD
           <Route exact path="/SignIn" conponent={SignIn} />
-=======
           <Route exact path="/footer" component={Footer} />
->>>>>>> 0dceebf15bf4c53cec96268ecacfc08bc82db38a
         </Switch>
       </Router>
     );
