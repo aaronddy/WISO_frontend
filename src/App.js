7c3ec0ba2c5f5079084b224716344564bb8a2766
daye Kang <aaronddy94@gmail.com>
2020-01-02 23:26:34 +0900

    modify SignIn

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import SignIn from "./Pages/SignIn/SignIn";
+
 
 class App extends Component {
   render() {
     return (
       <div>
-        <SignIn />
+        
       </div>
     );
   }
