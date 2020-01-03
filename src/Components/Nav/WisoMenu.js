2a9ac0a50d934c13a00f8d620757480f01fa70d6
daye Kang <aaronddy94@gmail.com>
2020-01-03 16:17:23 +0900

    conflict resolved

--- src/Components/Nav/WisoMenu.js
+++ src/Components/Nav/WisoMenu.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import "./Nav.scss";
-
+import wisoLogo from "../../Images/logoImages/wiso_blue_1.png";
 class WisoMenu extends Component {
   render() {
     return (
       <div className="tightAll">
         <div className="navigationGroup tightTwo">
           <a href="/">
-            <img
-              src="/Images/wiso_blue_1.png"
-              alt="wiso_logo"
-              className={"wisoLogoBlue"}
-            />
+            <img src={wisoLogo} alt="wiso_logo" className={"wisoLogoBlue"} />
           </a>
           <a href="#가사도우미">가사도우미</a>
           <a href="#이사">이사</a>
