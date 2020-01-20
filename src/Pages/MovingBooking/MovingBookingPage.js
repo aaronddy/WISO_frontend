2ca2e3b97cb91815c7701a1fe370239e674418ac
ykim5470-Lewis <58319204+ykim5470-Lewis@users.noreply.github.com>
2020-01-20 20:55:37 +0900

    이사 sms 연동

--- src/Pages/MovingBooking/MovingBookingPage.js
+++ src/Pages/MovingBooking/MovingBookingPage.js
@@ -1,10 +1,10 @@
-import React, { Component } from "react";
-import "./MovingBookingPage.scss";
-import MovingBookingComp from "./MovingBookingComp";
-import SelectOption from "./SelectOption";
-import DaumPostcode from "react-daum-postcode";
-import { withRouter } from "react-router-dom";
-import Nav from "../../Components/NavBlue/Nav";
+import React, { Component } from 'react';
+import './MovingBookingPage.scss';
+import MovingBookingComp from './MovingBookingComp';
+import SelectOption from './SelectOption';
+import DaumPostcode from 'react-daum-postcode';
+import { withRouter } from 'react-router-dom';
+import Nav from '../../Components/Nav/Nav';
 
 // import PostCode from "../../Components/PostcodeSearch/PostCode";
 // import { fullAddress } from "../../Components/PostcodeSearch/PostCode";
@@ -12,126 +12,137 @@ class MovingBookingPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Address: "",
-      phone: "",
+      Address: '',
+      phone: '',
       idp: false,
       mode1: false,
       mode2: false,
       mode3: false,
-      address: "",
-      isActived: false
+      address: '',
+      isActived: false,
+      token: '',
     };
   }
 
   handleData = data => {
     this.setState({
       isActived: !this.state.isActived,
-      address: data.address
+      address: data.address,
     });
   };
 
   handler = e => {
     console.dir(e.target);
-    if (e.target.innerText === "가정이사") {
+    if (e.target.innerText === '가정이사') {
       this.setState({
         mode1: true,
         mode2: false,
-        mode3: false
+        mode3: false,
       });
     }
-    if (e.target.innerText === "소형이사") {
+    if (e.target.innerText === '소형이사') {
       this.setState({
         mode1: false,
         mode2: true,
-        mode3: false
+        mode3: false,
       });
     }
-    if (e.target.innerText === "사무실이사") {
+    if (e.target.innerText === '사무실이사') {
       this.setState({
         mode1: false,
         mode2: false,
-        mode3: true
+        mode3: true,
       });
     }
   };
 
   goToMain = () => {
-    this.props.history.push("/");
+    this.props.history.push('/');
   };
   // idp1 만 클릭되었으면, idp1: false가 setState에서 true로 바뀐다.
 
   Bookingdone = () => {
+    this.setState({ token: localStorage.getItem('access_token') });
     if (this.state.mode1 === true) {
-      this.setState({ mode1: "1" });
+      this.setState({ mode1: '1' });
     }
     if (
-      this.state.mode1 === "1" &&
+      this.state.mode1 === '1' &&
       this.state.phone.length === 11 &&
       this.state.address !== undefined
     ) {
-      fetch("http://18.191.213.70:8080/move", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      fetch('http://10.58.0.176:8000/move', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: this.state.token,
+        },
         body: JSON.stringify({
-          user_id: "1",
-          move_categories_id: this.state.mode1,
+          user_id: '1',
+          movecategory_id: this.state.mode1,
           address: this.state.address,
-          phone_number: toString(this.state.phone)
-        })
+          phone_number: this.state.phone,
+        }),
       })
         .then(res => res.json())
         .then(res => {
-          alert("가정 이사 서비스를 문의 주셔서 감사합니다.");
+          alert('가정 이사 서비스를 문의 주셔서 감사합니다.');
           this.goToMain();
         });
     }
     if (this.state.mode2 === true) {
-      this.setState({ mode2: "2" });
+      this.setState({ mode2: '2' });
     }
     if (
-      this.state.mode2 === "2" &&
+      this.state.mode2 === '2' &&
       this.state.phone.length === 11 &&
       this.state.address !== undefined
     ) {
-      fetch("http://18.191.213.70:8080/move", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      fetch('http://10.58.0.176:8000/move', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: this.state.token,
+        },
         body: JSON.stringify({
-          user_id: "1",
-          move_categories_id: this.state.mode2,
+          user_id: '1',
+          movecategory_id: this.state.mode2,
           address: this.state.address,
-          phone_number: toString(this.state.phone)
-        })
+          phone_number: this.state.phone,
+        }),
       })
         .then(res => res.json())
         .then(res => {
-          alert("소형이사 서비스를 문의해주셔서 감사합니다.");
+          alert('소형이사 서비스를 문의해주셔서 감사합니다.');
           this.goToMain();
         });
 
       // this.goToMain.bind(this);
     }
     if (this.state.mode3 === true) {
-      this.setState({ mode3: "3" });
+      this.setState({ mode3: '3' });
     }
     if (
-      this.state.mode3 === "3" &&
+      this.state.mode3 === '3' &&
       this.state.phone.length === 11 &&
       this.state.address !== undefined
     ) {
-      fetch("http://18.191.213.70:8080/move", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
+      fetch('http://10.58.0.176:8000/move', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: this.state.token,
+        },
         body: JSON.stringify({
-          user_id: "1",
-          move_categories_id: this.state.mode3,
+          user_id: '1',
+          movecategory_id: this.state.mode3,
           address: this.state.address,
-          phone_number: toString(this.state.phone)
-        })
+          phone_number: this.state.phone,
+        }),
       })
         .then(res => res.json())
         .then(res => {
-          alert("사무실 서비스를 문의해주셔서 감사합니다.");
+          alert('사무실 서비스를 문의해주셔서 감사합니다.');
           this.goToMain();
         });
 
@@ -146,13 +157,13 @@ class MovingBookingPage extends Component {
     //   "width=500, height=300, left=460, top=280"
     // );
     this.setState({
-      isActived: !this.state.isActived
+      isActived: !this.state.isActived,
     });
   };
 
   FillAddress = () => {
     this.setState({
-      Address: this.props.fullAddress
+      Address: this.props.fullAddress,
     });
   };
 
@@ -166,96 +177,97 @@ class MovingBookingPage extends Component {
     // console.log(this.props);
     console.log(this.state.address);
     console.log(this.state.phone);
+    console.log(this.state.token);
 
     return (
-      <div className="MovingBookingPageTotal">
-        <nav className="navbar navbarDefault">
-          <div className="container pd">
-            <div className="navbarHeader">
+      <div className='MovingBookingPageTotal'>
+        <nav className='navbar navbarDefault'>
+          <div className='container pd'>
+            <div className='navbarHeader'>
               <Nav />
             </div>
           </div>
         </nav>
-        <div className="container formTitle">
-          <div className="appContainer">
+        <div className='container formTitle'>
+          <div className='appContainer'>
             <div>
-              <div className="container center">
+              <div className='container center'>
                 <MovingBookingComp
-                  atr="sectionHeading"
-                  heading="포장 이사 견적 문의"
+                  atr='sectionHeading'
+                  heading='포장 이사 견적 문의'
                 />
               </div>
-              <div className="formGroup">
+              <div className='formGroup'>
                 <hr></hr>
               </div>
             </div>
           </div>
-          <div className="container center sectionSubheading">
+          <div className='container center sectionSubheading'>
             <MovingBookingComp
-              atr="sectionSubheading"
-              heading="이사 종류를 선택해 주세요"
+              atr='sectionSubheading'
+              heading='이사 종류를 선택해 주세요'
             />
           </div>
-          <div className="center">
+          <div className='center'>
             <SelectOption
               clicked={this.handler}
               change={
                 this.state.mode1
-                  ? "moveButtonBlue center"
-                  : "moveButtonWraper center"
+                  ? 'moveButtonBlue center'
+                  : 'moveButtonWraper center'
               }
-              category="house"
-              service="가정이사"
+              category='house'
+              service='가정이사'
             />
             <SelectOption
               clicked={this.handler}
               change={
                 this.state.mode2
-                  ? "moveButtonBlue center"
-                  : "moveButtonWraper center"
+                  ? 'moveButtonBlue center'
+                  : 'moveButtonWraper center'
               }
-              category="house"
-              service="소형이사"
+              category='house'
+              service='소형이사'
             />
             <SelectOption
               clicked={this.handler}
               change={
                 this.state.mode3
-                  ? "moveButtonBlue center"
-                  : "moveButtonWraper center"
+                  ? 'moveButtonBlue center'
+                  : 'moveButtonWraper center'
               }
-              category="office"
-              service="사무실이사"
+              category='office'
+              service='사무실이사'
             />
           </div>
           <div>
             <a
-              className="outerFree"
-              href="http://rebrand.ly/80grh8"
-              target="_blank"
+              className='outerFree'
+              href='http://rebrand.ly/80grh8'
+              target='_blank'
             >
               <MovingBookingComp
-                atr="sectionPreheading center"
-                heading="서비스 상세 안내 보기"
+                atr='sectionPreheading center'
+                heading='서비스 상세 안내 보기'
               />
             </a>
           </div>
         </div>
-        <div className="container">
+        <div className='container'>
           <MovingBookingComp
-            atr="sectionSubheading center"
-            heading="서비스 받으실 주소 및 번호를 검색해 주세요"
+            atr='sectionSubheading center'
+            heading='서비스 받으실 주소 및 번호를 검색해 주세요'
           />
           <div>
-            <form className="bookingInfo center column container formTitle">
+            <form className='bookingInfo center column container formTitle'>
               <input
                 value={this.state.address}
-                type="text"
-                class="formControl inputName"
-                placeholder="주소"
+                type='text'
+                class='formControl inputName'
+                placeholder='주소'
               />
-              <div className="searchBtn">
-                <div onClick={this.SeAddress} className="center">
+              <div className='searchBtn'>
+                <div onClick={this.SeAddress} className='center'>
                   검색하기
                 </div>
               </div>
@@ -263,26 +275,26 @@ class MovingBookingPage extends Component {
                 <DaumPostcode onComplete={this.handleData} />
               )}
             </form>
-            <form className="bookingInfo center column container formTitle">
+            <form className='bookingInfo center column container formTitle'>
               <input
                 onChange={e => {
                   this.setState({
-                    phone: e.target.value
+                    phone: e.target.value,
                   });
                 }}
-                type="number"
-                class="formControl inputPhone"
-                placeholder="연락처"
+                type='number'
+                class='formControl inputPhone'
+                placeholder='연락처'
               ></input>
             </form>
           </div>
-          <div className="center">
+          <div className='center'>
             <button
-              type="button"
+              type='button'
               className={
                 this.state.address && this.state.phone.length === 11
-                  ? "btnAfter"
-                  : "btnBefore"
+                  ? 'btnAfter'
+                  : 'btnBefore'
               }
               onClick={this.Bookingdone}
             >
@@ -290,7 +302,7 @@ class MovingBookingPage extends Component {
             </button>
           </div>
         </div>
-        <div className="formGroup">
+        <div className='formGroup'>
           <hr></hr>
         </div>
         {/* Footer fixed scss will be fixed then I will import in here */}
